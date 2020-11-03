25d755fe0c21445a50f4fec5445e7d90e28f1d2d
andreikrivets <36642792+andreikrivets@users.noreply.github.com>
2020-11-03 23:03:54 +0300

    feat: add main app logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-function App() {
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import PageMain from './components/pages/PageMain/PageMain';
+import PageEmployers from './components/pages/PageEmployers/PageEmployers';
+import getUserData from './data/getUsersData';
+
+const App = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await getUserData();
+      setData(req);
+    }
+    fetchData()
+  }, [])
+  const handleDelete = id => setData((prev) => prev.filter(el => el.id !== id));
+  const handleCreate = info => setData((prev) => [...prev, info]);
+  console.log(data);
   return (
-    <div>
-      <h1>123</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+            <li>
+              <Link to="/employers">Сотрудники</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/employers">
+            {data.length ? <PageEmployers data={data} deleteEntry={handleDelete} createEntry={handleCreate}/> : null}
+          </Route>
+          <Route path="/">
+            <PageMain />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
