5ea8eaaf5ccd53d7e6234a53edc37c358fa1256a
andreikrivets <36642792+andreikrivets@users.noreply.github.com>
2020-11-03 23:01:55 +0300

    feat: add main page

---
+++ src/components/pages/PageMain/PageMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './PageMain.css'
+
+const PageMain = () => (
+    <section className="main">
+        <h1>Hello</h1>
+    </section>
+)
+
+export default PageMain
